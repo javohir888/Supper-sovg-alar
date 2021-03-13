6be6778ecec33940c10e1c050bc406f37f84ed85
javohir888 <javohirj686@gmail.com>
2021-03-14 01:55:44 +0500

    hello4

---
+++ index.js
@@ -0,0 +1,36 @@
+let boxOne = document.querySelector(".box1");
+let imageOne = document.querySelector(".box1 > img");
+boxOne.addEventListener('click', () => {
+    imageOne.parentNode.removeChild(imageOne);
+});
+
+let boxTwo = document.querySelector(".box2");
+let imageTwo = document.querySelector(".box2 > img");
+boxTwo.addEventListener('click', () => {
+    imageTwo.parentNode.removeChild(imageTwo);
+});
+
+let boxThree = document.querySelector(".box3");
+let imageThree = document.querySelector(".box3 > img");
+boxThree.addEventListener('click', () => {
+    imageThree.parentNode.removeChild(imageThree);
+});
+
+let boxFour = document.querySelector(".box4");
+let imageFour = document.querySelector(".box4 > img");
+boxFour.addEventListener('click', () => {
+    imageFour.parentNode.removeChild(imageFour);
+});
+
+let boxFive = document.querySelector(".box5");
+let imageFive = document.querySelector(".box5 > img");
+boxFive.addEventListener('click', () => {
+    imageFive.parentNode.removeChild(imageFive);
+});
+
+let boxsix = document.querySelector(".box6");
+let imagesix = document.querySelector(".box6 > img");
+boxsix.addEventListener('click', () => {
+    imagesix.parentNode.removeChild(imagesix);
+});
+
